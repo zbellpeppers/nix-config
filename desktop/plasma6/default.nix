0965512ca0438e4991e620d2c07a5e7b99222299
tkmockingbird <zbellpeppers@pm.me>
2025-01-21 15:18:18 -0500

    Added monochrome kde theme.

--- desktop/plasma6/default.nix
+++ desktop/plasma6/default.nix
@@ -1,34 +1,46 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  monochrome-sddm-theme = pkgs.stdenv.mkDerivation {
+    name = "monochrome-sddm-theme";
+    src = pkgs.fetchFromGitLab {
+      owner = "pwyde";
+      repo = "monochrome-kde";
+      rev = "master"; # Or a specific commit hash
+      hash = "sha256-mnEQQ2PHzARv65PSwGDyS2bcYlRUydD5HUmVD251Tts=";
+    };
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes
+      cp -r "$src/sddm/themes/monochrome" "$out/share/sddm/themes"
+    '';
+  };
+in {
   qt.platformTheme = "kde";
   environment.plasma6.excludePackages = [
     pkgs.kdePackages.oxygen
     pkgs.kdePackages.elisa
   ];
   services = {
-    # Display Manager Configuration
     displayManager = {
       sddm = {
         enable = true;
-        theme = "~/Documents/monochrome";
         wayland.enable = true;
         wayland.compositor = "kwin";
+        theme = "monochrome";
       };
     };
 
-    # Keyboard Mapping
     xserver = {
       xkb = {
         layout = "us,us";
         variant = "workman,";
         options = "grp:win_space_toggle,caps:capslock";
       };
-      # Configures How Fast Repeat Activates and How Often it Happens
       autoRepeatDelay = 275;
       autoRepeatInterval = 32;
     };
 
-    # Enable KDE Desktop Environment
     desktopManager.plasma6.enable = true;
     desktopManager.plasma6.enableQt5Integration = true;
   };
+
+  environment.systemPackages = [monochrome-sddm-theme];
 }
