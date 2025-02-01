df5e93459f7ba6bde0fec17790086324214a00c6
tkmockingbird <zbellpeppers@pm.me>
2025-02-01 08:50:35 -0500

    Added vivaldi.

--- hosts/king/home-manager/packages/default.nix
+++ hosts/king/home-manager/packages/default.nix
@@ -27,6 +27,12 @@ in {
     persepolis
     firedragon
     cheese
+    (vivaldi.overrideAttrs
+      (oldAttrs: {
+        dontWrapQtApps = false;
+        dontPatchELF = true;
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.kdePackages.wrapQtAppsHook];
+      }))
 
     # Windows Emulation
     bottles
