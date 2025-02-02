023aa2cbd1d8b46409a1ac8dad8106af9713db99
tkmockingbird <zbellpeppers@pm.me>
2025-02-02 16:27:17 -0500

    Changes made to update script

--- gitupdate.sh
+++ gitupdate.sh
@@ -36,44 +36,43 @@ echo "Nix-config Copied to /etc/nixos"
 if [[ " ${valid_commands[@]} " =~ " ${rebuild_type} " ]]; then
     if sudo nixos-rebuild "$rebuild_type"; then
         echo "NIXOS REBUILD COMPLETED"
+
+        if [ -f "/etc/nixos/flake.lock" ]; then
+            echo "Updating git's flake.lock"
+            sudo cp "/etc/nixos/flake.lock" "/home/$current_user/nix-config/flake.lock"
+            sudo chown $current_user:users "/home/$current_user/nix-config/flake.lock"
+        else
+            echo "No flake.lock found in /etc/nixos. Skipping flake.lock update."
+        fi
+
+        # Change ownership of the copied files to root
+        sudo chown -R root: "$nixos_dir/" || { echo "Ownership change failed"; exit 1; }
+        echo "Changed ownership of files in $nixos_dir to root."
+
+        # Change to Git directory
+        cd "$config_dir"
+        if [ ! -d ".git" ]; then
+            echo "Not a Git repository. Exiting."
+            exit 1
+        fi
+
+        # Git commit and push
+        if ! git diff-index --quiet HEAD --; then
+            read -p "Enter commit message: " commit_message
+            git add .
+            git commit -m "$commit_message"
+            git push origin master
+        else
+            echo "No changes to commit."
+        fi
     else
-        echo "NIXOS REBUILD FAILED"
+        echo "NIXOS REBUILD FAILED. Previous configuration restored."
+        sudo rm -rf "$nixos_dir"
+        sudo cp -r "$backup_dir/nixos" "$nixos_dir"
     fi
 else
     echo "Invalid rebuild type. Valid options are: ${valid_commands[*]}"
-fi
-
-
-if [ -f "/etc/nixos/flake.lock" ]; then
-    echo "Updating git's flake.lock"
-    sudo cp "/etc/nixos/flake.lock" "/home/$current_user/nix-config/flake.lock"
-    sudo chown $current_user:users "/home/$current_user/nix-config/flake.lock"
-else
-    echo "No flake.lock found in /etc/nixos. Skipping flake.lock update."
-fi
-
-# Change ownership of the copied files to root
-sudo chown -R root: "$nixos_dir/" || { echo "Ownership change failed"; exit 1; }
-echo "Changed ownership of files in $nixos_dir to root."
-
-# Change to Git directory
-cd "$config_dir"
-if [ ! -d ".git" ]; then
-    echo "Not a Git repository. Exiting."
-    exit 1
-fi
-
-# Git commit and push
-if ! git diff-index --quiet HEAD --; then
-    read -p "Enter commit message: " commit_message
-    git add .
-    git commit -m "$commit_message"
-    git push origin master
-else
-    echo "No changes to commit."
-fi
-else
-    echo "NIXOS REBUILD FAILED. Previous configuration restored."
+    echo "Previous configuration restored."
     sudo rm -rf "$nixos_dir"
     sudo cp -r "$backup_dir/nixos" "$nixos_dir"
 fi
