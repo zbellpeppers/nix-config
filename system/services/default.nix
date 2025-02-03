4dc1526f345b6fbe2f56f9f3002ee6c8a0430fae
tkmockingbird <zbellpeppers@pm.me>
2025-02-03 16:07:27 -0500

    Added back ollama and open-webui

--- system/services/default.nix
+++ system/services/default.nix
@@ -15,20 +15,19 @@
     acceleration = "rocm";
     rocmOverrideGfx = "10.3.0";
   };
-  services.nextjs-ollama-llm-ui.enable = true;
   # Open-WebUI - Web-based Interface
-  # services.open-webui = {
-  #   enable = true;
-  #   openFirewall = true;
-  #   stateDir = /var/lib/open-webui;
-  #   environment = {
-  #     ANONYMIZED_TELEMETRY = "False";
-  #     DO_NOT_TRACK = "True";
-  #     SCARF_NO_ANALYTICS = "True";
-  #     LLAMA_CPP_SERVER_URL = "http://127.0.0.1:8080";
-  #     WEBUI_AUTH = "False";
-  #   };
-  # };
+  services.open-webui = {
+    enable = true;
+    openFirewall = true;
+    stateDir = /var/lib/open-webui;
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+      WEBUI_AUTH = "False";
+    };
+  };
   # Btrfs Services
   services.btrfs = {
     autoScrub = {
