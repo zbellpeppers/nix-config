1ed05281db8e58cb68060b488757437ebf870d8a
tkmockingbird <zbellpeppers@pm.me>
2025-02-03 20:17:38 -0500

    Added deepseek to ollama. Added chromium to queen HM.

--- home-manager/queen/default.nix
+++ home-manager/queen/default.nix
@@ -5,6 +5,10 @@
   ...
 }: {
   imports = [../common];
+
+  home.packages = with pkgs; [
+    chromium
+  ];
   home.username = "sarah";
   home.homeDirectory = "/home/sarah";
   home.stateVersion = "24.11";
