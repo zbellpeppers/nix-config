241f32a1eb3e4cce45a0c1fc23caade508b44711
tkmockingbird <zbellpeppers@pm.me>
2025-02-05 07:47:20 -0500

    Testing brave, vivaldi

--- home-manager/king/default.nix
+++ home-manager/king/default.nix
@@ -3,11 +3,25 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  vivaldi =
+    pkgs.vivaldi.overrideAttrs
+    (oldAttrs: {
+      dontWrapQtApps = false;
+      dontPatchELF = true;
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.kdePackages.wrapQtAppsHook];
+    });
+in {
   imports = [
     ../common
     ./git
   ];
+
+  home.packages = with pkgs; [
+    brave
+    vivaldi
+  ];
+
   home.username = "zachary";
   home.homeDirectory = "/home/zachary";
   home.stateVersion = "24.11";
