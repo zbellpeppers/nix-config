4835c03801dc4c89c8c5fb0dc7871d0881ed9c9e
tkmockingbird <zbellpeppers@pm.me>
2025-02-07 10:48:59 -0500

    Switched nixpkgs from nixos-unstable to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Zach's Modular NixOS + Home Manager Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
