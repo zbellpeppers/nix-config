5dbe9c11856e13df13c9ee1526b8c3c9bb349f0d
tkmockingbird <zbellpeppers@pm.me>
2025-02-15 09:41:45 -0500

    Added gamescope args

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -10,7 +10,16 @@
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-      gamescopeSession.enable = true;
+      gamescopeSession = {
+        enable = true;
+        args = [
+          "-w 1920 -h 1080"
+          "--borderless"
+          "--adaptive-sync"
+          "--fullscreen"
+          "--mangoapp"
+        ];
+      };
     };
     zsh = {
       enable = true;
