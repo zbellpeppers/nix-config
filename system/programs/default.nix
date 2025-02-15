1441f46dc14daca6ea86c9278fabaad2492bcbe0
tkmockingbird <zbellpeppers@pm.me>
2025-02-15 10:30:58 -0500

    Added proton-ge-custom for steam

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -10,6 +10,9 @@
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      extraCompatPackages = [
+        pkgs.proton-ge-bin
+      ];
       gamescopeSession = {
         enable = true;
       };
