ad8b28845fb8d549f6b3a7413f945dc0c97ef89e
tkmockingbird <zbellpeppers@pm.me>
2025-02-19 09:30:08 -0500

    System won't build. Sick of integration hell

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Zach's Modular NixOS + Home Manager Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "path:/home/zachary/projects/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -41,7 +41,6 @@
       };
       overlays = [
         nix-vscode-extensions.overlays.default
-        (import ./overlays/grub-fix.nix)
       ];
     };
   in {
