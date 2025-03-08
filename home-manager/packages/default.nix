a047253ebdff8caa4da634701ef6819c0d428922
tkmockingbird <zbellpeppers@pm.me>
2025-03-08 08:04:57 -0500

    Added vivaldi. Lowes website breaks without chromium.

--- home-manager/packages/default.nix
+++ home-manager/packages/default.nix
@@ -20,6 +20,13 @@ in {
     # Browser Related
     persepolis
     xclicker
+    (vivaldi.overrideAttrs
+      (oldAttrs: {
+        dontWrapQtApps = false;
+        dontPatchELF = true;
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.kdePackages.wrapQtAppsHook];
+      }))
+    vivaldi-ffmpeg-codecs
 
     # Entertainment
     vlc
