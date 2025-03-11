cad53c08bc05755de58aaedac9ea748c4daa2719
tkmockingbird <zbellpeppers@pm.me>
2025-03-11 18:40:49 -0400

    Trying to fix fish

--- home-manager/common/fish/default.nix
+++ home-manager/common/fish/default.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
   programs.fish = {
-    enable = true;
     plugins = [
       {
         name = "z";
@@ -34,6 +33,15 @@
       {
         name = "puffer";
         src = pkgs.fishPlugins.puffer;
+      }
+      {
+        name = "projectdo";
+        src = pkgs.fetchFromGitHub {
+          owner = "paldepind";
+          repo = "projectdo";
+          rev = "main";
+          sha256 = "sha256-ZBAzHo7/Sy8uyjWganSFs/uOJkmuDP8WxU3WVGttPTo=";
+        };
       }
       {
         name = "virtualfish";
@@ -120,9 +128,63 @@
       egrep = "egrep --color=auto";
       hw = "hwinfo --short";
       jctl = "journalctl -p 3 -xb";
-      upgit = "/home/$(whoami)/nix-config/gitupdate.sh";
+      upgit = "/home//nix-config/gitupdate.sh";
       garbage = "sudo nix-collect-garbage -d";
-      upflake = "cd /home/$(whoami)/nix-config && nix flake update";
+      upflake = "cd /home/zachary/nix-config && nix flake update";
     };
+    interactiveShellInit = ''
+      # Functions needed for !! and !$
+      function __history_previous_command
+        switch (commandline -t)
+        case "!"
+          commandline -t $history[1]
+          commandline -f repaint
+        case "*"
+          commandline -i !
+        end
+      end
+
+      function __history_previous_command_arguments
+        switch (commandline -t)
+        case "!"
+          commandline -t ""
+          commandline -f history-token-search-backward
+        case "*"
+          commandline -i '$'
+        end
+      end
+
+      # Change fish suggestions color
+      set fish_color_autosuggestion '#a6e8ff'
+
+      if [ "$fish_key_bindings" = fish_vi_key_bindings ]
+        bind -Minsert ! __history_previous_command
+        bind -Minsert '$' __history_previous_command_arguments
+      else
+        bind ! __history_previous_command
+        bind '$' __history_previous_command_arguments
+      end
+
+      # Fish command history
+      function history
+        builtin history --show-time='%F %T '
+      end
+
+      function backup --argument filename
+        cp $filename $filename.bak
+      end
+
+      # Copy DIR1 DIR2
+      function copy
+        set count (count $argv | tr -d \n)
+        if test "$count" = 2; and test -d "$argv[1]"
+          set from (echo $argv[1] | trim-right /)
+          set to (echo $argv[2])
+          command cp -r $from $to
+        else
+          command cp $argv
+        end
+      end
+    '';
   };
 }
