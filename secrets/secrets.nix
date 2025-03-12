030b6ce4d077f14f91696980f1849659d404b8c7
lqmbies <sarahnsmallwood@proton.me>
2025-03-12 12:21:18 -0400

    Updated queen public keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,11 @@
 #TODO Update Sarah and Queen's user and system ssh keys
 let
   zachary = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBaRxUhfTxYDc9D4ebtKfbwh0Kt7J0vQA24wZUwHvOpz";
-  sarah = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILI6jSq53F/3hEmSs+oq9L4TwOo1PrDMAgcA1uo1CCV/";
+  sarah = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0gPiWyXr6KB5RjkAsfEqGZp5VozMF9gawDH+tjfiMg";
   users = [zachary sarah];
 
   king = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFV6gaDEzfSEaVjXFCh4epG6UkT+dbGrChxU5humq6s1";
-  queen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzxQgondgEYcLpcPdJLrTdNgZ2gznOHCAxMdaceTUT1";
+  queen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqPNjO8VCHW2DCxOBJWcrBpet/hB++ohhQIYRDF+lAl";
   systems = [king queen];
 in {
   "actualbudget-password.age".publicKeys = [zachary sarah];
