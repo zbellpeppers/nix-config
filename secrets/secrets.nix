3ae7f972a7efb0e3ecef602604b29508fbe26983
lqmbies <sarahnsmallwood@proton.me>
2025-03-12 12:29:13 -0400

    Updated new keys. I think I need to rekey the secrets from pc that created them with the updated keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,10 @@
 let
   zachary = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBaRxUhfTxYDc9D4ebtKfbwh0Kt7J0vQA24wZUwHvOpz";
-  sarah = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILI6jSq53F/3hEmSs+oq9L4TwOo1PrDMAgcA1uo1CCV/";
+  sarah = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJtMs0C0iUNAoWGt3BDQk4iv7mFu8HWCqCMKFKT4n9A";
   users = [zachary sarah];
 
   king = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFV6gaDEzfSEaVjXFCh4epG6UkT+dbGrChxU5humq6s1";
-  queen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzxQgondgEYcLpcPdJLrTdNgZ2gznOHCAxMdaceTUT1";
+  queen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqPNjO8VCHW2DCxOBJWcrBpet/hB++ohhQIYRDF+lAl";
   systems = [king queen];
 in {
   "actualbudget-password.age".publicKeys = [zachary sarah];
