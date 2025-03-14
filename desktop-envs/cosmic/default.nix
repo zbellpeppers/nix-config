3c6c60f886d03e5d4800a4c8aa89df2fe669c0c8
tkmockingbird <zbellpeppers@pm.me>
2025-03-14 07:54:20 -0400

    Trying to add services.redshift so my eyes stop melting.

--- desktop-envs/cosmic/default.nix
+++ desktop-envs/cosmic/default.nix
@@ -1,4 +1,22 @@
-{...}: {
+{pkgs, ...}: {
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
+  environment.systemPackages = with pkgs; [
+  ];
+  location.provider = "manual";
+  location.latitude = 33.734497;
+  location.longitude = -84.111847;
+  services.redshift = {
+    enable = true;
+    # location.latitude = 33.734497;
+    # location.longitude = -84.111847;
+    brightness = {
+      day = "1";
+      night = "1";
+    };
+    temperature = {
+      day = 2800;
+      night = 2800;
+    };
+  };
 }
