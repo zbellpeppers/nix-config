f6375a3c2b31a82042637a0cb2779a7d11beddea
tkmockingbird <zbellpeppers@pm.me>
2025-03-15 22:53:31 -0400

    nixos - build #20250315-225302

--- hosts/default.nix
+++ hosts/default.nix
@@ -77,39 +77,39 @@ in {
   king = mkHost {
     hostname = "king";
     username = "zachary";
-    desktop = "gnome";
+    desktop = "plasma";
     modules = [
     ];
-    overlays = [
-      (final: prev: {
-        mutter = prev.mutter.overrideAttrs (oldAttrs: {
-          # GNOME dynamic triple buffering (huge performance improvement)
-          # See https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1441
-          # Also https://gitlab.gnome.org/vanvugt/mutter/-/tree/triple-buffering-v4-47
-          src = final.fetchFromGitLab {
-            domain = "gitlab.gnome.org";
-            owner = "vanvugt";
-            repo = "mutter";
-            rev = "triple-buffering-v4-47";
-            hash = "sha256-6n5HSbocU8QDwuhBvhRuvkUE4NflUiUKE0QQ5DJEzwI=";
-          };
+    # overlays = [
+    #   (final: prev: {
+    #     mutter = prev.mutter.overrideAttrs (oldAttrs: {
+    #       # GNOME dynamic triple buffering (huge performance improvement)
+    #       # See https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1441
+    #       # Also https://gitlab.gnome.org/vanvugt/mutter/-/tree/triple-buffering-v4-47
+    #       src = final.fetchFromGitLab {
+    #         domain = "gitlab.gnome.org";
+    #         owner = "vanvugt";
+    #         repo = "mutter";
+    #         rev = "triple-buffering-v4-47";
+    #         hash = "sha256-6n5HSbocU8QDwuhBvhRuvkUE4NflUiUKE0QQ5DJEzwI=";
+    #       };
 
-          # GNOME 47 requires the gvdb subproject which is not included in the triple-buffering branch
-          # This copies the necessary gvdb files from the official GNOME repository
-          preConfigure = let
-            gvdb = final.fetchFromGitLab {
-              domain = "gitlab.gnome.org";
-              owner = "GNOME";
-              repo = "gvdb";
-              rev = "2b42fc75f09dbe1cd1057580b5782b08f2dcb400";
-              hash = "sha256-CIdEwRbtxWCwgTb5HYHrixXi+G+qeE1APRaUeka3NWk=";
-            };
-          in ''
-            cp -a "${gvdb}" ./subprojects/gvdb
-          '';
-        });
-      })
-    ];
+    #       # GNOME 47 requires the gvdb subproject which is not included in the triple-buffering branch
+    #       # This copies the necessary gvdb files from the official GNOME repository
+    #       preConfigure = let
+    #         gvdb = final.fetchFromGitLab {
+    #           domain = "gitlab.gnome.org";
+    #           owner = "GNOME";
+    #           repo = "gvdb";
+    #           rev = "2b42fc75f09dbe1cd1057580b5782b08f2dcb400";
+    #           hash = "sha256-CIdEwRbtxWCwgTb5HYHrixXi+G+qeE1APRaUeka3NWk=";
+    #         };
+    #       in ''
+    #         cp -a "${gvdb}" ./subprojects/gvdb
+    #       '';
+    #     });
+    #   })
+    # ];
   };
 
   # Queen host
