380edd9e3f38bc28c41780fedbf7318628ab1b3b
tkmockingbird <zbellpeppers@pm.me>
2025-03-17 16:03:00 -0400

    Working on headscale

--- hosts/king/server/headscale/default.nix
+++ hosts/king/server/headscale/default.nix
@@ -8,7 +8,6 @@ in {
   services = {
     tailscale = {
       enable = true;
-      port = 0;
       useRoutingFeatures = "both";
       openFirewall = true;
     };
@@ -16,16 +15,33 @@ in {
     headscale = {
       enable = true;
       address = "0.0.0.0";
+      port = 8080;
       user = "headscale";
       group = "headscale";
-      port = 8080;
+      # tls_letsencrypt_hostname = "headscale.yourdomain.com";
+      # tls_letsencrypt_challenge_type = "HTTP-01";
+      # tls_letsencrypt_listen = ":http";
       settings = {
         log.level = "debug";
         log.format = "text";
+        noise.private_key_path = "/var/lib/headscale/noise_private.key";
         server_url = "https://headscale.${domain}";
         dns = {
           magic_dns = true;
           base_domain = "${domain}";
+          nameservers.global = ["1.1.1.1" "8.8.8.8"];
+        };
+        prefixes = {
+          v4 = "100.64.0.0/10"; # Standard Tailscale IPv4 CIDR
+          v6 = "fd7a:115c:a1e0::/48"; # Example IPv6 CIDR
+          allocation = "sequential"; # Can be "random" or "sequential"
+        };
+        database = {
+          type = "sqlite";
+          sqlite = {
+            path = "/var/lib/headscale/db.sqlite";
+            write_ahead_log = true; # Recommended for performance
+          };
         };
       };
     };
@@ -39,5 +55,8 @@ in {
       };
     };
   };
-  environment.systemPackages = [config.services.headscale.package];
+  # environment.systemPackages = with pkgs; [
+  #   headscale
+  #   tailscale
+  # ];
 }
