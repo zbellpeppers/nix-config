70aaa40a60d840408c2ab045b459b1f6f7954053
tkmockingbird <zbellpeppers@pm.me>
2025-03-18 07:55:26 -0400

    nixos - build #20250318-075514

--- home-manager/common/fish/default.nix
+++ home-manager/common/fish/default.nix
@@ -128,17 +128,6 @@
         end
       end
 
-      # Change fish suggestions color
-      # set fish_color_autosuggestion '#6aa84f'
-
-      if [ "$fish_key_bindings" = fish_vi_key_bindings ]
-        bind -Minsert ! __history_previous_command
-        bind -Minsert '$' __history_previous_command_arguments
-      else
-        bind ! __history_previous_command
-        bind '$' __history_previous_command_arguments
-      end
-
       # Fish command history
       function history
         builtin history --show-time='%F %T '
