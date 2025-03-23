8d4939a83f8fdde192354a116c5407dbe275c2a7
zbellpeppers <codeberg@bell-peppers.com>
2025-03-23 09:13:20 -0400

    Secrets issues

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   sarah = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJtMs0C0iUNAoWGt3BDQk4iv7mFu8HWCqCMKFKT4n9A";
   users = [zachary sarah];
 
-  king = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcVjJwbAGu5Hxp6wnIjdCv94PK5mg/tN2hFRIFMCxkA";
+  king = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxEVmsQlJ6DV32h6ol0jXjc9aRL0PPPvx5CNhhuy66O";
   queen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqPNjO8VCHW2DCxOBJWcrBpet/hB++ohhQIYRDF+lAl";
   systems = [king queen];
 in {
