33cc9aee9b07e55af2f1a1301af5e30cdff3f5ce
zbellpeppers <codeberg@bell-peppers.com>
2025-03-25 10:42:43 -0400

    Adjusted caddy. Removed some headscale config stuff

--- hosts/king/server/caddy/default.nix
+++ hosts/king/server/caddy/default.nix
@@ -7,12 +7,8 @@
     virtualHosts."actualbudget.bell-peppers.com" = {
       extraConfig = ''
         # This enables automatic HTTPS
-
         # Reverse proxy to your Actual Budget container
         reverse_proxy 127.0.0.1:5006 {
-          # Forward original host and protocol information
-          header_up X-Forwarded-Port 443
-          header_up X-Forwarded-Proto https
         }
       '';
     };
