b5e29fa16f923b0b8e831ccf90d8975c53a7062b
zbellpeppers <github@bell-peppers.com>
2025-03-26 07:31:50 -0400

    Rekeying secrets

--- hosts/king/server/ddclient/default.nix
+++ hosts/king/server/ddclient/default.nix
@@ -3,16 +3,16 @@
     enable = true;
     protocol = "cloudflare";
     server = "api.cloudflare.com/client/v4";
-    username = "token"; # Your Cloudflare account email
-    passwordFile = config.age.secrets.cloudflare-dns-apitoken.path; # File containing just your API token
+    username = "token"; # token if using apitoken or Cloudflare email
+    passwordFile = config.age.secrets.cloudflare-dns-apitoken.path;
     zone = "bell-peppers.com"; # Your root domain
     domains = [
       "actualbudget.bell-peppers.com"
     ]; # The subdomain to update
     ssl = true;
     interval = "10min"; # Check every 10 minutes
-    usev4 = "webv4, webv4=https://cloudflare.com/cdn-cgi/trace, webv4-skip=ip="; # Use web service to detect IPv4
-    usev6 = "webv6, webv6=https://cloudflare.com/cdn-cgi/trace, webv6-skip=ip="; # Use web service to detect IPv6
+    usev4 = "webv4, webv4=https://cloudflare.com/cdn-cgi/trace, webv4-skip=ip=";
+    usev6 = "webv6, webv6=https://cloudflare.com/cdn-cgi/trace, webv6-skip=ip=";
     verbose = true; # For debugging, can set to false later
     quiet = false; # For debugging, can set to true later
     extraConfig = ''
