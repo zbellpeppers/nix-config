85a747cf9f37c34b8f89ad5a157fb2eddffe3b73
zbellpeppers <github@bell-peppers.com>
2025-03-30 13:13:31 -0400

    nixos - build #20250330-131307

--- system/printer/default.nix
+++ system/printer/default.nix
@@ -20,10 +20,6 @@
     enable = true;
     drivers = [
       pkgs.canon-cups-ufr2
-      #TODO An old temporary fix for the canon cups driver error - Remove 04-11-2025
-      # (pkgs.canon-cups-ufr2.overrideAttrs (oldAttrs: {
-      #   dontCheckForBrokenSymlinks = true;
-      # }))
     ];
   };
   # SCANNER CONFIGURATION
