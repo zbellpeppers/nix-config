d5b8643c10584baa9192d1e6793eb624d5440a4c
zbellpeppers <github@bell-peppers.com>
2025-04-15 07:51:30 -0400

    nixos - build #20250415-075109

--- hosts/king/server/caddy/default.nix
+++ hosts/king/server/caddy/default.nix
@@ -5,11 +5,9 @@
     email = "zbellpeppers@pm.me";
     # Reverse proxy for actual budget server.
     virtualHosts."actualbudget.bell-peppers.com" = {
-      listenAddresses = [
-        "localhost:5006"
-      ];
       extraConfig = ''
         encode gzip zstd
+        reverse_proxy localhost:5006
       '';
     };
   };
